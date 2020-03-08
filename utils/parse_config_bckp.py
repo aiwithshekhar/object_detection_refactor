22b7a2bef0c1180668f0f2107e8a4a827c3e045e
aiwithshekhar <sharma.shashank509@gmail.com>
2020-03-08 22:54:42 +0530

    8th march working on datasets

---
+++ utils/parse_config_bckp.py
@@ -0,0 +1,38 @@
+import os
+import numpy as np
+def parse_model_cfg(path):
+    with open(path, 'r') as f:
+        lines = f.read().split('\n')
+    lines = [x for x in lines if x and not x.startswith('#')]
+    lines = [x.rstrip().lstrip() for x in lines]  # get rid of fringe whitespaces
+    mdefs = []  # module definitions
+    for line in lines:
+        if line.startswith('['):  # This marks the start of a new block
+            mdefs.append({})
+            mdefs[-1]['type'] = line[1:-1].rstrip()
+            if mdefs[-1]['type'] == 'convolutional':
+                mdefs[-1]['batch_normalize'] = 0  # pre-populate with zeros (may be overwritten later)
+        else:
+            key, val = line.split("=")
+            key = key.rstrip()
+
+            if 'anchors' in key:
+                mdefs[-1][key] = np.array([float(x) for x in val.split(',')]).reshape((-1, 2))  # np anchors
+            else:
+                mdefs[-1][key] = val.strip()
+    print (mdefs)
+    return mdefs
+
+def parse_data_cfg(path):
+    with open(path, 'r') as f:
+        lines = f.readlines()
+
+    options = dict()
+    for line in lines:
+        line = line.strip()
+        if line == '' or line.startswith('#'):
+            continue
+        key, val = line.split('=')
+        options[key.strip()] = val.strip()
+    print (f'cv is {options}')
+    return options
